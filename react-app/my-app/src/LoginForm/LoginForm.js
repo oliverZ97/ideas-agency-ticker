cfac8801a22d2c5af62d5ccfb83bfbde59c9cf7d
Oliver Ziemann <oliver@mordoor.de>
2019-04-15 14:38:23 +0200

    Add LoginButtonFunctionality
    If the User clicks on the Login-Button it sends the Input Values of the Name and the Password Inputfield

--- react-app/my-app/src/LoginForm/LoginForm.js
+++ react-app/my-app/src/LoginForm/LoginForm.js
@@ -3,21 +3,48 @@ import { Link } from 'react-router';
 import './LoginForm.css';
 
 class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      password: ''
+    };
+
+    this.handleChangeUsername = this.handleChangeUsername.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeUsername(e) {
+    console.log(e);
+    this.setState({ name: e.target.value })
+  }
+
+  handleChangePassword(e) {
+    console.log(e);
+    this.setState({ password: e.target.value })
+
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
   render() {
     return (
-        <form className="LoginForm">
-          <div className="username">
-            <label htmlFor="uName"></label>
-            <input type="text" placeholder="Username" minLength="4" name="uName"></input>
-          </div>
-          <div className="password">
-            <label htmlFor="psw"></label>
-            <input type="password" placeholder="Password" minLength="6" name="psw"></input>
-          </div>
-          <div>
-          <Link to="/list"><button type="submit">Login</button></Link>
-          </div>
-        </form>
+      <form className="LoginForm" onSubmit={this.handleSubmit}>
+        <div className="username">
+          <label htmlFor="uName"></label>
+          <input type="text" placeholder="Username" minLength="4" name="uName" onChange={this.handleChangeUsername}></input>
+        </div>
+        <div className="password">
+          <label htmlFor="psw"></label>
+          <input type="password" placeholder="Password" minLength="6" name="psw" onChange={this.handleChangePassword}></input>
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
     );
   }
 }
