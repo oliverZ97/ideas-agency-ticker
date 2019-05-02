675c85642ce9500779c5682d76f77966028fd544
Oliver Ziemann <oliver@mordoor.de>
2019-05-02 09:08:37 +0200

    add redirection of navBar Buttons

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -8,6 +8,9 @@ class Navigation extends Component {
   constructor() {
     super();
     this.handleLogout = this.handleLogout.bind(this);
+    this.handleFilterClick = this.handleFilterClick.bind(this);
+    this.handleSearchClick = this.handleSearchClick.bind(this);
+    this.handleFavClick= this.handleFavClick.bind(this);
     this.authentificationService = authentificationService
   }
 
@@ -16,14 +19,26 @@ class Navigation extends Component {
     browserHistory.push('/login');
   }
 
+  handleFilterClick() {
+    browserHistory.push('/filter');
+  }
+
+  handleSearchClick() {
+    browserHistory.push('/ssearches');
+  }
+
+  handleFavClick() {
+    browserHistory.push('/favorites');
+  }
+
   render() {
     return (
       <div className="Navigation">
         <div className="nav_content">
           <div className="nav_buttons">
-            <a href="filter"><button id="navigation_button_filter" type="submit"></button></a>
-            <a href="ssearches"><button id="navigation_button_searches" type="submit"></button></a>
-            <a href="favorites"><button id="navigation_button_favs" type="submit"></button></a>
+            <a href="filter"><button id="navigation_button_filter" onClick={this.handleFilterClick}></button></a>
+            <a href="ssearches"><button id="navigation_button_searches" onClick={this.handleSearchClick}></button></a>
+            <a href="favorites"><button id="navigation_button_favs" onClick={this.handleFavClicks}></button></a>
           </div>
           <div className="nav_logout">
             <button id="navigation_button_logout" onClick={this.handleLogout}></button>
