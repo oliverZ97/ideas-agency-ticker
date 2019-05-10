c481e12fa7f26d43e09c841d38635512726fbf7b
Oliver Ziemann <oliver@mordoor.de>
2019-05-10 15:39:20 +0200

    add mediaQuery for Tile__headline

--- src/Components/ShortDate/ShortDate.js
+++ src/Components/ShortDate/ShortDate.js
@@ -6,7 +6,7 @@ function ShortDate(props) {
     let newDate = dStr.substring(0, dStr.indexOf(':', dStr.indexOf(':')+1))
 
     return (
-        <div className="text text--light headline headline__tertiary">{newDate || ''}</div>
+        <div className="Tile__headline">{newDate || ''}</div>
     );
 }
 
