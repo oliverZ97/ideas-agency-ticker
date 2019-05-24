1b3603e15c28bdc68f9935dfbb716ee0949d8991
Oliver Ziemann <oliver@mordoor.de>
2019-05-24 17:39:36 +0200

    change web.js

--- web.js
+++ web.js
@@ -1,15 +1,7 @@
 var express = require('express');
 var app = express();
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('build'));
-} else {
-    app.use(express.static('dist')); //set directory with static files
-}
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-});
+app.use(express.static('dist')); //set directory with static files
 
 const PORT = process.env.PORT || 3000;
 
