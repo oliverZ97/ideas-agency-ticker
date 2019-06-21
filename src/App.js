cdd0960c2e43fdd5649c5cb6261fe04fe37b3ca3
Oliver Ziemann <oliver@mordoor.de>
2019-06-21 12:50:01 +0200

    change styleguide path to link from github pages

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import FavPage from './Pages/FavPage/FavPage';
 import SaveSearchPage from './Pages/SaveSearchPage/SaveSearchPage';
 import ListPageSearches from './Pages/ListPageSearches/ListPageSearches';
 
-import '../../scssComments2HTML/docs/styleguide.min.css';
 import '../src/scss/styles.scss';
 
 
